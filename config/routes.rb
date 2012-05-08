81bd21a116d47777cac5742a014b2962179f595b
gdemir <gdemir@bil.omu.edu.tr>
2012-05-08 16:45:11 +0300

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,88 @@
 Medical::Application.routes.draw do
+
+  root :to => 'home#index'
+
+  match "home" => "home#index"
+  match "about" => "home#about"
+  match "contact" => "home#contact"
+
+  get   "home/index"
+  post  "home/index"
+  get   "home/consult_register"
+  post  "home/consult_register"
+  get   "home/consult_query"
+  post  "home/consult_query"
+  get   "home/patient_query"
+  post  "home/patient_query"
+  get   "home/department_query"
+  post  "home/department_query"
+  match "home/consult_destroy" => "home#consult_destroy"
+  match "home/consult_destroy/:id/:patient_id" => "home#consult_destroy"
+
+  match "/admin" => "admin#index"
+
+  namespace :admin do
+    get  :login
+    get  :logout
+    post :sign_in
+
+    get  :index
+    get  :profile
+    post :profile_update
+    get  :help
+
+    resources :drugs do
+      get :destroy
+    end
+    post "drugs/update"
+
+    resources :departments do
+      get :destroy
+    end
+    post "departments/update"
+
+    resources :patients do
+      get :destroy
+    end
+    post "patients/update"
+
+    match "consults/all" => "consults#all"
+    resources :consults do
+      get :destroy
+    end
+
+    resources :doctors do
+      get :destroy
+    end
+    post "doctors/update"
+
+    resources :trial_types do
+      get :destroy
+    end
+    post "trial_types/update"
+
+    match "trials/:trial_type_id/new" => "trials#new"
+    resources :trials do
+      get :destroy
+    end
+    post "trials/update"
+
+    resources :admins do
+      get :destroy
+    end
+    post "admins/update"
+
+    resources :invoices do
+      get :destroy
+    end
+    post "invoices/update"
+
+    resources :trial_results
+    resources :trial_requests
+
+    resources :people
+  end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
